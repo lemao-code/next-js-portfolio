0770ab8b3d91de86aa70ee1a495ef75d108c8b6c
lemao-code <th.ferrari@outlook.com.br>
2021-02-07 21:46:50 -0300

    new layout

---
+++ pages/style.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+
+
+export const Container = styled.div ` 
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+export const Conteudo  = styled.main `
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-top: 100px;
+`
+
+export const Header = styled.header `
+    margin-top: 80px;
+    margin-left: 80px;
+    display: flex;
+    flex-direction: row;
+`
+
+
+export const SobreMim = styled.div `
+    width: 600px;
+    height: 500px;
+    background-color: black;
+    margin-left: 30px;
+    margin-left: 100px;
+    padding: 10px;
+`
+
+export const NavBar = styled.div ` 
+    width: auto;
+    height: auto;
+    margin-left: 900px;
+    display: flex;
+    flex-direction: row;
+`
+
+export const Footer = styled.footer `
+    margin-top: 80px;
+    width: 100%;
+    height: 20px;
+    border-top: 1px solid white;
+`
